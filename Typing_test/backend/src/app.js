5425f2b392452c0379e5f1f5692d9373cae83b58
kamaleshap346@gmail.com <kamaleshap346gmail.com>
2026-01-23 15:53:30 +0530

    chore: setup express server with nodemon and env config

---
+++ Typing_test/backend/src/app.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+
+const app=express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+app.get("/health",(req,res)=>{
+    res.status(200).json({status:"Ok",msg:"backend is runnign"})
+})
+
+export default app
